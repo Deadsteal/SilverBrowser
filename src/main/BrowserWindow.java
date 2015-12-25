46fb4bb3de7d3571576d2a8d642416f6d0119527
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-12-25 18:16:30 -0500

    Jsoup for getting webpages is too good

--- src/main/BrowserWindow.java
+++ src/main/BrowserWindow.java
@@ -16,6 +16,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import javax.swing.*;
 
+import org.jsoup.Jsoup;
+
 public class BrowserWindow extends JFrame {
 
 	// URL/search bar at top
@@ -64,60 +66,82 @@ public class BrowserWindow extends JFrame {
 		searchButton = new JButton("Go");
 		searchButton.setPreferredSize(new Dimension(85, 50));
 
+		displayPane = new JEditorPane();
+		displayPane.setContentType("text/html");
+		displayPane.setPreferredSize(new Dimension(this.getWidth() - 20, this.getHeight() - 150));
+		
 		// put the go button next to the search bar in a cheeky manner
 		layoutManager.putConstraint(SpringLayout.WEST, searchButton, 5, SpringLayout.EAST, searchBar);
 		
 //		// the searchbutton will always be 5 px away from the edge of the window
 //		layoutManager.putConstraint(SpringLayout.EAST, searchButton, 0, SpringLayout.EAST, this);
-		
+
 		// the searchbar will always be 5 px away from the edge of the window
 		layoutManager.putConstraint(SpringLayout.WEST, searchBar, 5, SpringLayout.WEST, this);
+
+		layoutManager.putConstraint(SpringLayout.NORTH, displayPane, 5, SpringLayout.SOUTH, searchBar);
+//		layoutManager.putConstraint(SpringLayout.EAST, displayPane, 5, SpringLayout.EAST, this);
+//		layoutManager.putConstraint(SpringLayout.WEST, displayPane, 5, SpringLayout.WEST, this);
 				
 		
 		this.add(searchBar);
 		this.add(searchButton);
-
+		this.add(displayPane);
+		
 		this.setVisible(true);
 		
 		// test
+		String display = "";
 		try {
-			sendGetRequest("http://www.google.com");
+			display = sendGetRequest("http://facebook.com/");
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+//		displayPane.setText(display);
 		
 	} // end of constructor
 	
-	private void sendGetRequest(String url) throws Exception {
+	private String sendGetRequest(String url) throws Exception {
 		
-		URL urlObj = new URL(url);
+//		URL urlObj = new URL(url);
+//		
+//		HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+//		
+//		// add request header and assorted garbage
+//		connection.setRequestMethod("GET");
+//		connection.setRequestProperty("User-Agent", "silverbrowser.1"); // TODO: Fix this?
+//		connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5"); // make sure the response is in english
+//	
+//		// what response did the server send?
+//		int responseCode = connection.getResponseCode();
+//		System.out.println("Response code from " + url + " was " + Integer.toString(responseCode));
+//	
+//		// read the response
+//		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//	
+//		String inputLine;
+//		StringBuffer responseBuffer = new StringBuffer();
+//		
+//		while ((inputLine = in.readLine()) != null) {
+//			responseBuffer.append(inputLine);
+//		}
+//		in.close();
+//		
+//		String response = responseBuffer.toString();		
+//		
+//		Jsoup.
+//		
 		
-		HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+		// Jsoup reduced all this code to 1 line
+		String html = Jsoup.connect(url).get().html();
+		System.out.println(html);
+		displayPane.setText(html);
+		return html;
 		
-		// add request header and assorted garbage
-		connection.setRequestMethod("GET");
-		connection.setRequestProperty("User-Agent", "silverbrowser.1"); // TODO: Fix this?
-		connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5"); // make sure the response is in english
-	
-		// what response did the server send?
-		int responseCode = connection.getResponseCode();
-		System.out.println("Response code from " + url + " was " + Integer.toString(responseCode));
-	
-		// read the response
-		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-	
-		String inputLine;
-		StringBuffer responseBuffer = new StringBuffer();
-		
-		while ((inputLine = in.readLine()) != null) {
-			responseBuffer.append(inputLine);
-		}
-		in.close();
-		
-		String response = responseBuffer.toString();
-		
-		System.out.println(response);
+//		System.out.println(response);
+//		
+//		return response;
 		
 	}  // end of sendGetRequest
 	
